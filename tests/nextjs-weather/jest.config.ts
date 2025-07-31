f4d285a04e62630a8a6fc71166b89523a5f90c97
lol <rp@gm.com>
2025-07-31 12:49:11 +0000

    improved test experience

--- tests/nextjs-weather/jest.config.ts
+++ tests/nextjs-weather/jest.config.ts
@@ -1,8 +1,8 @@
 import type { Config } from "jest";
 
 export default {
-	displayName: "fullstack-nextjs-weather",
-	coverageDirectory: "../../coverage/apps/fullstack-nextjs-weather",
+	displayName: "weather",
+	coverageDirectory: "../../coverage/tests/nextjs-weather",
 	preset: "../../jest.preset.js",
 	moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
 } satisfies Config;
